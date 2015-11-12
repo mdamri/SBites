b07fb8fb9ecdf6db64edb6e3f6bb1a2586eae07e
TOSHIBA <amirazaafouri@gmail.com>
2015-11-12 16:29:49 +0100

    test zaafouri

--- app/src/main/java/com/example/a_omri/helloworld/MainActivity.java
+++ app/src/main/java/com/example/a_omri/helloworld/MainActivity.java
@@ -15,7 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+//test Zaafouri
 public class MainActivity extends Activity {
 
     TextView registerScreen;
