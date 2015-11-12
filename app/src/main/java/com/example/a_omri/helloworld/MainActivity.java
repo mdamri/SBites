54af46ab3854d6e2b1fee1e58c8b58a8ad0217f0
Administrateur <ababacarsyll@gmail.com>
2015-11-12 16:40:22 +0100

    Syll ababacar

--- app/src/main/java/com/example/a_omri/helloworld/MainActivity.java
+++ app/src/main/java/com/example/a_omri/helloworld/MainActivity.java
@@ -15,8 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-//TEST SYLL ABABABCAR
+//TEST SYLL ABABABCAR @A.SylL
 public class MainActivity extends Activity {
 
     TextView registerScreen;
