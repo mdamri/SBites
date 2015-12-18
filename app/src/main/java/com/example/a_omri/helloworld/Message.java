7c21c9719167bed445c4ba3ea4a3082f7b64fb5d
Administrateur <ababacarsyll@gmail.com>
2015-12-18 15:03:47 +0100

    New Version

---
+++ app/src/main/java/com/example/a_omri/helloworld/Message.java
@@ -0,0 +1,37 @@
+package com.example.a_omri.helloworld;
+
+/**
+ * Created by Ababacar Syll on 17/12/2015.
+ */
+public class Message {
+    String user;
+    String msg;
+    String date;
+
+    public Message() {
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
